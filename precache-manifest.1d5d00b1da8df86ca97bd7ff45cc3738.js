0459d9228fa667bea32e1c90e7e68769d8766bd6
Arek <majkhrhr@gmail.com>
2019-04-13 10:27:51 +0200

    Updates

---
+++ precache-manifest.1d5d00b1da8df86ca97bd7ff45cc3738.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "38e233f10b94c997b715",
+    "url": "/REACT-e-commerce-shop-local-products/static/css/main.193f4d89.chunk.css"
+  },
+  {
+    "revision": "38e233f10b94c997b715",
+    "url": "/REACT-e-commerce-shop-local-products/static/js/main.7ca38bfe.chunk.js"
+  },
+  {
+    "revision": "5098b9aa19f09b80435e",
+    "url": "/REACT-e-commerce-shop-local-products/static/js/runtime~main.01537aa1.js"
+  },
+  {
+    "revision": "b705c97ce7ec48e458ff",
+    "url": "/REACT-e-commerce-shop-local-products/static/css/2.7b5eeba4.chunk.css"
+  },
+  {
+    "revision": "b705c97ce7ec48e458ff",
+    "url": "/REACT-e-commerce-shop-local-products/static/js/2.c4810c78.chunk.js"
+  },
+  {
+    "revision": "ad6fd4d2e8bf105cb411296aa290ee29",
+    "url": "/REACT-e-commerce-shop-local-products/static/media/logo.ad6fd4d2.png"
+  },
+  {
+    "revision": "0560fb16085549e9cb90806bc3788c49",
+    "url": "/REACT-e-commerce-shop-local-products/static/media/fields1.0560fb16.jpg"
+  },
+  {
+    "revision": "4c11e616a320e3c7262aea4a9105b3cc",
+    "url": "/REACT-e-commerce-shop-local-products/index.html"
+  }
+];
